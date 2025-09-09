4e0af003ca99252efcc367fe8bbd0604ff00739d
Andre Wagner <andre.nho@gmail.com>
2025-09-08 19:15:38 -0500

    .

--- neblina/main/config.cc
+++ neblina/main/config.cc
@@ -2,6 +2,7 @@
 
 #include <filesystem>
 #include <iostream>
+#include <fstream>
 namespace fs = std::filesystem;
 
 #include "embedded.hh"
@@ -9,13 +10,12 @@ namespace fs = std::filesystem;
 
 Config::Config(int argc, char* argv[])
 {
-    std::string config_path = std::string(getenv("HOME")) + "/.config/neblina";
-    std::string config_filename = "neblina.conf";
+    std::string config_filename = default_config_file_path() + "/neblina.conf";
 
     parse_arguments(argc, argv);
 
-    if (!fs::exists(config_path))
-        create_config_file();
+    if (!fs::exists(config_filename))
+        create_config_file(config_filename);
 
     load_config_file();
 }
@@ -25,13 +25,22 @@ void Config::parse_arguments(int argc, char* argv[])
     // TODO - optget
 }
 
-void Config::create_config_file()
+void Config::create_config_file(std::string const& filepath)
 {
     std::string default_config_file = decode_embedded_text(default_config, sizeof default_config, default_config_uncompressed_sz);
-    std::cout << default_config_file;
+    fs::create_directory(default_config_file_path());
+    std::ofstream out(filepath, std::ios::out | std::ios::trunc);
+    if (!out)
+        throw std::runtime_error("Could not create default config file");
+    out << default_config_file;
 }
 
 void Config::load_config_file()
 {
 
+}
+
+std::string Config::default_config_file_path()
+{
+    return std::string(getenv("HOME")) + "/.config/neblina";
 }
--- neblina/main/config.hh
+++ neblina/main/config.hh
@@ -11,8 +11,9 @@ private:
     std::string config_filename;
 
     void parse_arguments(int argc, char* argv[]);
-    void create_config_file();
+    void create_config_file(std::string const& filepath);
     void load_config_file();
+    static std::string default_config_file_path();
 };
 
 #endif //CONFIG_HH
