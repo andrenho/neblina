01b6e5308c9781e3bf2927f701eb016dbb62e5bc
Andre Wagner <andre.nho@gmail.com>
2025-09-09 15:05:08 -0500

    .

--- neblina/main.cc
+++ neblina/main.cc
@@ -6,13 +6,9 @@
 
 int main(int argc, char* argv[])
 {
-    /*
     ConfigManager config(argc, argv);
     if (config.service.empty())
         Watchdog(config).run();
     else
         std::cout << "Started service " << config.service << std::endl;
-    */
-    TCPServer t(23456);
-    t.run();
 }
