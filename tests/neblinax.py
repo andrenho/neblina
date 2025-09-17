2f55f39d27b7215e988c2f028f6b8c355bab47a6
Andre Wagner <WagnerAndre@JohnDeere.com>
2025-09-17 15:14:20 -0500

    .

--- tests/neblinax.py
+++ tests/neblinax.py
@@ -3,7 +3,7 @@ import time
 
 
 def start():
-    p = subprocess.Popen(["./neblina", "-D", "../init"], cwd="../../neblina")
+    p = subprocess.Popen(["./neblina", "-D", "../tests/init"], cwd="../neblina")
     time.sleep(0.5)
     assert p.poll() is None  # process is still running
     return p
