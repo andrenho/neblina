584273def527b5ef2ac8245dd036f109d92bda99
Andre Wagner <andre.nho@gmail.com>
2025-09-23 08:53:45 -0500

    .

--- src/main/error.h
+++ src/main/error.h
@@ -3,7 +3,11 @@
 
 #define NON_RECOVERABLE_ERROR 57
 
-void err_non_recoverable(const char* fmt, ...) __attribute__ ((format (printf, 1, 2)));
-void err(const char* fmt, ...) __attribute__ ((format (printf, 1, 2)));
+#ifndef _MSC_VER
+#  define ATTR_PRINTF(a, b) __attribute__ ((format (printf, a, b)))
+#endif
+
+void err_non_recoverable(const char* fmt, ...) ATTR_PRINTF(1, 2);
+void err(const char* fmt, ...) ATTR_PRINTF(1, 2);
 
 #endif
